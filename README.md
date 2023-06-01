95bad87b673118fc57de66a1e9ae4f147b62d6bb
MXJXN <modulations@gmail.com>
2023-06-01 14:42:32 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## a command line openai chatbot.
+
+### Run it in a terminal: 
+
+first, in a .env file, set `OPENAI_API_KEY`.
+
+then run: `npm i && node index.js`
+
+### commands
+
+`clear`: clears chat history
+
+`exit`: quits the process but you can always use `<C-c>`
+
+This is especially nice if you use tmux and want a single panel dedicated to the chatbot.
+At least thats why I made it.
---
+++ index.js
@@ -0,0 +1,49 @@
+// Import dependencies
+const { Configuration, OpenAIApi } = require("openai");
+const readline = require("readline");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
+
+// Configure OpenAI API
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// Create readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Function to handle user input and API calls
+async function chatWithGPT3(prompt, chatHistory) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: chatHistory + "\nYou: " + prompt + "\nGPT-3: ",
+    max_tokens: 150,
+  });
+  return completion.data.choices[0].text.trim();
+}
+
+// Main chat loop
+function chatLoop(chatHistory = "") {
+  rl.question("You: ", async (input) => {
+    if (input.toLowerCase() === "exit") {
+      rl.close();
+    } else if (input.toLowerCase() === "clear") {
+      chatLoop(""); // Clear chat history
+    } else {
+      const response = await chatWithGPT3(input, chatHistory);
+      const updatedChatHistory = chatHistory + "\nYou: " + input + "\nGPT-3: " + response;
+      console.log("GPT-3: " + response);
+      chatLoop(updatedChatHistory);
+    }
+  });
+}
+
+// Start chat loop
+console.log("Type 'exit' to end the conversation. Type 'clear' to clear chat history.");
+chatLoop();
